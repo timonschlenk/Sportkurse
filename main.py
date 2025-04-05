2df08eaae89a19ca819bc5a6f2bb14ce38c933b2
Timon Schlenk <timon.schlenk@herr-der-mails.de>
2025-04-05 18:12:07 +0200

    changed a bit to inscription page but can't acces the page yet

--- main.py
+++ main.py
@@ -5,31 +5,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+# Import the time library
 import time
 
-# Import the Chrome WebDriver
+# Setup Chrome options and service
 service = Service(executable_path='./chromedriver.exe')
-
-# Create a new instance of the Chrome driver
 driver = webdriver.Chrome(service=service)
 
-# Open a website
-driver.get('https://www.google.com')
+#Execute the script
+driver.get('https://www.dhsz.tu-dresden.de/angebote/aktueller_zeitraum/_Doppelkopf.html') # Open the Doppelkopf page
 
-# Wait for the page to load
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS, 'gLFyf'))
+# # Wait for the link to be present on the page
+# link_to_inscription = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.LINK_TEXT, 'buchen')) # ID of the link
+# )
+
+# Wait for the link to be present on the page
+link_to_inscription = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, 'Jonas Kunigkeit')) # ID of the link
 )
 
-# Find the input element using its name attribute value
-input_element = driver.find_element(By.CLASS_NAME, 'gLFyf')
-
-# Type 'Selenium' into the input element
-input_element.clear()  # Clear the input field if needed
-input_element.send_keys('Selenium' + Keys.ENTER)
-
-# Print the title of the page  
-print(driver.title)
+link_to_inscription.click()
 
 # Wait for 5 seconds
 time.sleep(5)
